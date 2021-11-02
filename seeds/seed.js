3a94e4e2356650c4dd465258237ff3cef4d1e353
Denis Arce <dnsarce21@gmail.com>
2021-11-02 17:11:33 -0500

    added seed files

---
+++ seeds/seed.js
@@ -0,0 +1,19 @@
+const sequelize = require("../config/connection");
+const { User, Pet } = require("../models");
+const userData = require("./userData.json");
+const petData = require("./petData.json");
+
+const seedDatabase = async () => {
+  await sequelize.sync({ force: true });
+  await User.bulkCreate(userData, {
+    individualHooks: true,
+    returning: true
+  });
+  await Pet.bulkCreate(petData, {
+    individualHooks: true,
+    returning: true
+  });
+
+  process.exit(0);
+};
+seedDatabase();
