a70c66ba8d36979e968c0d3e5d7c3852be3d374b
Denis Arce <dnsarce21@gmail.com>
2021-11-05 13:42:06 -0500

    added register and profile hbars and js

--- controllers/homeRoute.js
+++ controllers/homeRoute.js
@@ -12,8 +12,8 @@ router.get("/", async (req, res) => {
 
     res.render("homepage", {
       users,
-      logged_in: req.session.logged_in,
-      text: "i am working"
+      logged_in: req.session.logged_in
+      // text: "i am working"
     });
   } catch (err) {
     res.status(500).json(err);
@@ -22,6 +22,18 @@ router.get("/", async (req, res) => {
 // router.get("/", async (req, res) =>
 //   res.render("homepage", { text: "i am working" })
 // );
+
+router.get("/register", async (req, res) => {
+  if (req.session.logged_in) {
+    res.redirect("/profile");
+    return;
+  }
+
+  res.render("register");
+});
+
+module.exports = router;
+
 router.get("/login", async (req, res) => {
   if (req.session.logged_in) {
     res.redirect("/");
